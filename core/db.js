e24e0a42217ed7c0eb5ad5fde74b31d2cd462a7d
ilmikko <mikko@5x.fi>
2017-09-16 14:15:24 +0100

    Added db.js

---
+++ core/db.js
@@ -0,0 +1,115 @@
+/*
+ *	db.js
+ *	Connects to all the three databases and requests a single query from them
+ *	Returns a JSON file in the format:
+ *	{
+ *		"aliss":<data from ALISS>,
+ *		"gcd":<data from GCD>,
+ *		"milo":<data from MILO>
+ *	}
+ *
+ *	Exports:
+ *		db.query(<querystring>,<callback>);
+ *		for example:
+ *		db.query("cancer",function(json){
+ *			console.log(json.milo.hits);
+ *		})
+ *
+ * */
+
+const http=require('http'),https=require('https');
+const fs=require('fs');
+
+function Race(callback){
+	this.running=0;
+	this.finished=false;
+	this.callback=callback||function(){};
+}
+Race.prototype={
+	start:function(f){
+		if (!this.finished) this.running++;
+		let self=this;
+		if (f) f(function(){
+			self.finish();
+		});
+	},
+	finish:function(){
+		if (!this.finished&&--this.running==0){
+			this.finished=true;
+			this.callback();
+		}
+	}
+};
+
+module.exports={
+	query:function(query,callback){
+		var results={};
+
+		var race=new Race(function(){
+			// All queries have finished
+			console.log('All queries finished!');
+			fs.writeFileSync('results.json',JSON.stringify(results));
+		});
+
+		race.start(function(finish){
+			// ALISS
+			https.get({
+				path:"/api/v2/search/?q="+query,
+				hostname:"www.aliss.org",
+				method:"GET",
+				headers:{}
+			},function(res){
+				console.log('Status '+res.statusCode);
+				console.log('Headers '+JSON.stringify(res.headers));
+				let data='';
+				res.on('data',function(chunk){
+					data+=chunk;
+				});
+				res.on('end',function(){
+					try{
+						results.aliss=JSON.parse(data);
+					}
+					catch(err){
+						console.error('ALISS error: '+err);
+					}
+					finish();
+				});
+			});
+		});
+
+		race.start(function(finish){
+			// MILO
+			https.get({
+				path:"/web-content/milo-organisation/_search?q="+query,
+				hostname:"50896fdf5c15388f8976945e5582a856.eu-west-1.aws.found.io",
+				method:"GET",
+				headers:{
+					'Authorization':"Basic cmVhZG9ubHk6b25seXJlYWQ="
+				}
+			},function(res){
+				console.log('Status '+res.statusCode);
+				console.log('Headers '+JSON.stringify(res.headers));
+				let data='';
+				res.on('data',function(chunk){
+					data+=chunk;
+				});
+				res.on('end',function(){
+					try{
+						results.milo=JSON.parse(data);
+					}
+					catch(err){
+						console.error('ALISS error: '+err);
+					}
+					finish();
+				});
+			});
+		});
+
+		race.start(function(finish){
+			// GCD
+			// TODO
+			results.gcd=null;
+			finish();
+		});
+	}
+};
