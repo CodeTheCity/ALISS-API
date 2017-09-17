3e530649e588f6e10a0e5c3736e823bc53441102
ilmikko <mikko@5x.fi>
2017-09-17 15:29:10 +0100

    We now shuffle the array so that we don't prefer aliss as the first item

--- parser.js
+++ parser.js
@@ -6,6 +6,17 @@ var parsers={
 	'gcd':gcd
 };
 
+function distance(x1,y1,x2,y2){
+	return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+
+function shuffle(a){
+	for (let i = a.length; i; i--) {
+		let j = Math.floor(Math.random() * i);
+		[a[i - 1], a[j]] = [a[j], a[i - 1]];
+	}
+}
+
 module.exports={
 	aggregateServices:function(json){
 		var services=[];
@@ -20,6 +31,11 @@ module.exports={
 				}
 			}else console.warn('No parser found for service \''+service+'\'!');
 		}
+
+		// Sort services, do some mixing and matching
+		// Right now we just shuffle the array
+		shuffle(services);
+
 		return services;
 	}
 };
