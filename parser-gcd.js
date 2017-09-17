2f3252aa18e40713348c31b0f40f4666e4a170f5
ilmikko <mikko@5x.fi>
2017-09-17 10:05:12 +0100

    parser-gcd aggregate template

--- parser-gcd.js
+++ parser-gcd.js
@@ -1,6 +1,30 @@
 module.exports={
 	parse:function(gcd){
         // TODO
-        return gcd;
+        	return {
+			"_id":null,
+			"_service":"gcd-todo",
+			"title":null,
+			"description":null,
+			"additionalInformation":null,
+			"tags":[],
+			"contact":{
+				"email":null,
+				"phone":null,
+				"facebook":null,
+				"twitter":null,
+				"website":null
+			},
+			"location":{
+				"coordinates":{
+					"latitude":null,
+					"longitude":null
+				},
+				"postcode":null,
+				"street":null,
+				"city":null,
+				"country":null
+			}
+		};
 	}
 };
