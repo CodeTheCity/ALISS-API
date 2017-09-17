2be05eca64f55bad9c1990fce65d96454ed2a533
ilmikko <mikko@5x.fi>
2017-09-17 15:42:55 +0100

    Added error handling for ALISS and GCD

--- db-aliss.js
+++ db-aliss.js
@@ -1,37 +1,40 @@
 module.exports = {
-    getDetails:function(id,callback){
-        callback({});
-    },
-    query:function(qo,callback){
-        var query=qo.query,area=qo.area;
-        let path = "/api/v2/search/?q="+encodeURIComponent(query);
+	getDetails:function(id,callback){
+		callback({});
+	},
+	query:function(qo,callback){
+		var query=qo.query,area=qo.area;
+		let path = "/api/v2/search/?q="+encodeURIComponent(query);
 
-        if(area){
-            path += `&latitude=${area.lat}&longitude=${area.lon}&distance=${area.distance}`;
-        }
+		if(area){
+			path += `&latitude=${area.lat}&longitude=${area.lon}&distance=${area.distance}`;
+		}
 
-        https.get({
-            path:path,
-            hostname:"www.aliss.org",
-            method:"GET",
-            headers:{
-                'Accept':'application/json'
-            }
-        },function(res){
-            console.log('Status '+res.statusCode);
-            console.log('Headers '+JSON.stringify(res.headers));
-            let data='';
-            res.on('data',function(chunk){
-                data+=chunk;
-            });
-            res.on('end',function(){
-                try{
-                    callback(JSON.parse(data));
-                }
-                catch(err){
-                    console.error('ALISS error: '+err);
-                }
-            });
-        });
-    }
+		https.get({
+			path:path,
+			hostname:"www.aliss.org",
+			method:"GET",
+			headers:{
+				'Accept':'application/json'
+			}
+		},function(res){
+			console.log('Status '+res.statusCode);
+			console.log('Headers '+JSON.stringify(res.headers));
+			let data='';
+			res.on('data',function(chunk){
+				data+=chunk;
+			});
+			res.on('end',function(){
+				try{
+					callback(JSON.parse(data));
+				}
+				catch(err){
+					console.error('ALISS error: '+err);
+				}
+			});
+		}).on('error',function(err){
+			console.error(err);
+			callback({});
+		});
+	}
 };
